f429ca378f4b9cdb985385e3afc96f7362197792
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-10-03 09:37:42 -0400

    Add project files.

---
+++ Topic - 7.2 For Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Topic___7._2_For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countTo;
+            int countBy;
+            int countFrom;
+            Console.WriteLine("count to number");
+            int.TryParse(Console.ReadLine(), out countTo);
+            
+            for (int i = 1; i <= countTo; i = i + 1)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("number counter");
+            Console.Write("count to: ");
+            int.TryParse(Console.ReadLine(), out countTo);
+            Console.Write("count by: ");
+            int.TryParse((string)Console.ReadLine(), out countBy);
+            Console.Write("count from: ");
+            int.TryParse(Console.ReadLine(), out countFrom);
+            for (int i = countFrom; i <= countTo; i = i + countBy)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
