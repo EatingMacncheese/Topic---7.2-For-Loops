93e0361f5a9763cd1ad8f750ab37db4a935e4d66
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-10-04 08:35:59 -0400

    Now its done

--- Topic - 7.2 For Loops/Program.cs
+++ Topic - 7.2 For Loops/Program.cs
@@ -7,6 +7,20 @@
             int countTo;
             int countBy;
             int countFrom;
+            string favFood;
+
+            Console.WriteLine("whats one of your favorite foods");
+            favFood = Console.ReadLine();
+            Console.WriteLine("how much do you like " + favFood + " on a scale of 1 to 10");
+            
+            int.TryParse(Console.ReadLine(), out countTo);
+
+            for (int i = 1; i <= countTo; i = i + 1)
+            {
+                Console.WriteLine(i + favFood);
+            }
+            Console.ReadLine();
+
             Console.WriteLine("count to number");
             int.TryParse(Console.ReadLine(), out countTo);
             
